93f3b4fad29550cb17993c16d2a8e51cb4a686fc
doomymax <doomymax@gmail.com>
2022-02-06 20:24:05 +0300

    add test with textarea

--- test/test_hexlet_code.rb
+++ test/test_hexlet_code.rb
@@ -5,18 +5,29 @@ require "test_helper"
 class TestHexletCode < Minitest::Test
   include FixtureHelper
 
-  User = Struct.new(:name, :job, keyword_init: true)
+  User = Struct.new(:name, :job,:gender, keyword_init: true)
 
   def test_that_it_has_a_version_number
     refute_nil ::HexletCode::VERSION
   end
 
-  def test_generate_form_inputs
-    user = User.new job: "hexlet"
-    expected = File.new("./test/fixtures/form-submit.html").read.chomp
+  # def test_generate_form_inputs
+  #   user = User.new job: "hexlet"
+  #   expected = File.new("./test/fixtures/form-submit.html").read.chomp
+  #   actual = HexletCode.form_for user do |f|
+  #     f.input :name
+  #     f.input :job
+  #     f.submit
+  #   end
+  #   assert { expected == actual }
+  # end
+
+  def test_generate_form_inputs_with_textarea
+    user = User.new name: 'rob', job: 'hexlet', gender: 'm'
+    expected = File.new("./test/fixtures/form-submit-textarea.html").read.chomp
     actual = HexletCode.form_for user do |f|
       f.input :name
-      f.input :job
+      f.input :job, as: :text
       f.submit
     end
     assert { expected == actual }
