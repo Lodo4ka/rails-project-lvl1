3b54c4845163b4e77f639fa39b3bb1e2a9fb127d
doomymax <doomymax@gmail.com>
2022-02-06 23:25:46 +0300

    fix lint for new test

--- lib/hexlet_code/form_builder.rb
+++ lib/hexlet_code/form_builder.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require "hexlet_code/tags.rb"
+require "hexlet_code/tags"
 
 module HexletCode
   class FormBuilder
--- test/test_hexlet_code.rb
+++ test/test_hexlet_code.rb
@@ -5,7 +5,7 @@ require "test_helper"
 class TestHexletCode < Minitest::Test
   include FixtureHelper
 
-  User = Struct.new(:name, :job,:gender, keyword_init: true)
+  User = Struct.new(:name, :job, :gender, keyword_init: true)
 
   def test_that_it_has_a_version_number
     refute_nil ::HexletCode::VERSION
@@ -23,7 +23,7 @@ class TestHexletCode < Minitest::Test
   end
 
   def test_generate_form_inputs_with_textarea
-    user = User.new name: 'rob', job: 'hexlet', gender: 'm'
+    user = User.new name: "rob", job: "hexlet", gender: "m"
     expected = File.new("./test/fixtures/form-submit-textarea.html").read.chomp
     actual = HexletCode.form_for user do |f|
       f.input :name
@@ -34,7 +34,7 @@ class TestHexletCode < Minitest::Test
   end
 
   def test_generate_form_inputs_with_input_class
-    user = User.new name: 'rob', job: 'hexlet', gender: 'm'
+    user = User.new name: "rob", job: "hexlet", gender: "m"
     expected = File.new("./test/fixtures/forms-submit-class.html").read.chomp
     actual = HexletCode.form_for user do |f|
       f.input :name, class: "user-input"
